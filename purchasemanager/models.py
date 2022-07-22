1f5746ed7d5d82d509636b1f34c2a5342f0cfaa9
Giovanni Mugno <49875306+LeoJuanZ@users.noreply.github.com>
2022-07-22 10:44:58 -0400

    app models

--- purchasemanager/models.py
+++ purchasemanager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Clients(models.Model):
+    name = models.CharField(max_length=30)
+    adress = models.CharField(max_length=100)
+    email = models.EmailField()
+    tlf = models.CharField(max_length=7)
+
+class Items(models.Model):
+    name = models.CharField(max_length=30)
+    category = models.CharField(max_length=20)
+    price = models.IntegerField()
+
+class CashOrder(models.Model):
+    number =  models.IntegerField()
+    date = models.DateField()
+    delivered = models.BooleanField()
+
